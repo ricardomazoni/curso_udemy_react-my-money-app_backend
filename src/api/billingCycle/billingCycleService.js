16b85761ab0895177573a4d836f4602d5172a845
Ricardo Mazoni <ricardomazoni@gmail.com>
2018-05-24 10:02:44 -0300

    retirado os comentarios do billingCycles

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -33,22 +33,6 @@ BillingCycle.methods(['get','post','put','delete'])
    * so recebe a contabilização
    */
 
-//   BillingCycle.route('summary', (req, res, next) => {
-//     BillingCycle.aggregate({
-//         $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
-//     }, { 
-//         $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
-//     }, {
-//         $project: {_id: 0, credit: 1, debt: 1}
-//     }, (error, result) => {
-//         if(error) {
-//             res.status(500).json({errors: [error]})
-//         } else {
-//             res.json(result[0] || { credit: 0, debt: 0 })
-//         }
-//     })
-// })   
-
 BillingCycle.route('summary', (req, res, next) => {
     BillingCycle.aggregate([{
         $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
